5c8a1aa729a98a2480a1dc88ee469d86bc1373ab
yuchan1120 <nogginpanic@gmail.com>
2023-03-26 12:28:48 +0900

    add: think_menu機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'top#index'
+  root 'think_menu#interview_yesterday_meal'
+
+  get 'interview_yesterday_meal', to: 'think_menu#interview_yesterday_meal'
+  post 'interview_yesterday_meal', to: 'think_menu#interview_yesterday_meal_submit'
+  get 'suggest_today_meal', to: 'think_menu#suggest_today_meal'
+
 end
